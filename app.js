61fdb00647b9477836da409fdef08612f6017447
Andres <andrescruz.tech@gmail.com>
2018-09-16 19:37:07 -0300

    Campground Show Page.

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
@@ -20,7 +21,8 @@ var Campground = mongoose.model('Campground', campgroundSchema);
 // TEMPORAL
 // Campground.create({
 //     name: 'Granite Hill',
-//     image: 'https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ebb0b2d80f4b2d82f3587492b80e2321&auto=format&fit=crop&w=750&q=80'
+//     image: 'https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ebb0b2d80f4b2d82f3587492b80e2321&auto=format&fit=crop&w=750&q=80',
+//     description: 'Testing comment.'
 // }, function(err, campground){
 //     if(err){
 //         console.log(err);
@@ -32,28 +34,29 @@ var Campground = mongoose.model('Campground', campgroundSchema);
 
 // ===== ROUTES =====
 
-// Index
 app.get('/', function(req, res){
     res.render('landing');
 });
 
-// Campgrounds
+// INDEX - shows all campgrounds
 app.get('/campgrounds', function(req, res){
     // Get all campgrounds from DB
     Campground.find({}, function(err, AllCampgrounds){
         if(err){
             console.log(err);
         } else {
-            res.render('campgrounds', {campgrounds: AllCampgrounds});
+            res.render('index', {campgrounds: AllCampgrounds});
         }
     });
 });
 
+// CREATE - adds campgrounds to DB
 app.post('/campgrounds', function(req, res){
     // get data from form
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err) {
@@ -65,11 +68,23 @@ app.post('/campgrounds', function(req, res){
     })    
 });
 
+// NEW - shows form to create new campground
 app.get('/campgrounds/new', function(req, res){
     res.render('new.ejs');
 });
 
-
+// SHOW - shows more info about one campground
+app.get('/campgrounds/:id', function(req, res){
+    // find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            // render show template with that campground
+            res.render('show', {campground: foundCampground});
+        }
+    });
+});
 
 
 
