b3337d2f710e001bde3c8c24e7c801bce24d2a21
Andres <andrescruz.tech@gmail.com>
2018-09-30 09:53:36 -0300

    Campground Edit and Update.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
+var methodOverride = require('method-override');
 Campground = require('./models/campground');
 Comment = require('./models/comment');
 User = require('./models/user');
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 // seedDB(); // seed the database
 
 // === PASSPORT CONFIG ===
