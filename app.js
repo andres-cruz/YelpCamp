ef223650addefcd7ec1fa13f7e918fc1cbc89e5a
Andres <andrescruz.tech@gmail.com>
2018-10-07 01:28:59 -0300

    Improved db path.

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes = require('./routes/comments'),
     indexRoutes = require('./routes/index');
 
 // Config
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL;
+mongoose.connect(url, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
